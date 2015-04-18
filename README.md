567dd2694b514321b09c076e77a9f08d9736e5d9
Raffaele <izziaraffaele@gmail.com>
2015-04-18 21:21:54 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# generator-silex-app 
+
+> [Yeoman](http://yeoman.io) generator for a Silex based web app. It creates a simple web application based on Silex PHP framework with 2 routes ("/login", "/signup"). It also includes React, browserify, SASS support, Bootstrap and gulp.
+
+## What's inside?
+
+Bundled:
+
+* Silex
+* Gulp
+* Bower
+* jQuery
+* Browserify
+* Reactify - Help to transform JSX
+* watchify support!
+* livereload (BrowserSync)
+
+Optional:
+
+* Sass with Compass
+* Bootstrap - Twitter Bootstrap's official Sass version
+* Modernizr
+* Jest for unit tests
+
+## Getting Started
+
+```
+$ npm install -g yo                                # Install Yeoman (if you don't have it yet)...
+$ npm install -g generator-webcomposer             # ...then install this generator...
+$ yo webcomposer                                   # ...and run it.
+```
+
+You'll need to install it with `gem install sass`.
+If you find your css build results are empty, update your sass gem.
+
+Now, when everything is ready, run the `watch` task and begin to develop your application.
+```
+$ gulp watch
+```
+
+The `dist` task helps you preparing your file for the live deploy, minifying and uglifyng both CSS and JS
+
+```
+$ gulp dist
+```
+
+How to run test?  
+Currently, I prefer to run test tasks from npm. Please run this command.
+```
+$ npm test
+```
+
+## License
+
+MIT
---
+++ app/index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+
+var Generator = yeoman.generators.Base.extend({
+    init: function () {
+        this.pkg = yeoman.file.readJSON(path.join(__dirname, '../package.json'));
+        this.on('end', function () {
+            if (!this.options['skip-install']) {
+                this.installDependencies();
+            }
+        });
+    },
+    askFor: function () {
+        var done = this.async();
+
+        // have Yeoman greet the user
+        console.log(this.yeoman);
+
+        // replace it with a short and sweet description of your generator
+        console.log(chalk.magenta('You\'re using A Yeoman Generator for a Silex based web app!!'));
+
+        var prompts = [{
+            name: 'project',
+            message: 'What is this project\'s name?'
+        },
+        {
+            type: 'checkbox',
+            name: 'features',
+            message: 'What more would you like?',
+            choices: [{
+                name: 'Auth system',
+                value: 'includeAuth',
+                checked: true
+            }]
+        }];
+
+        this.prompt(prompts, function (answers) {
+            var features = answers.features;
+
+            this.projectName = answers.project || 'myApp';
+
+            function hasFeature(feat) { return features.indexOf(feat) !== -1; }
+
+            // manually deal with the response, get back and store the results.
+            // we change a bit this way of doing to automatically do this in the self.prompt() method.
+            this.includeAuth = hasFeature('includeAuth');
+
+            done();
+        }.bind(this));
+    },
+    templates: function() {
+        this.composeWith('silex-skelethon', { projectName:this.projectName},{
+            local: require.resolve('generator-silex-skelethon')
+        });
+        this.composeWith('webcomposer-frontend', { projectName:this.projectName},{
+            local: require.resolve('generator-webcomposer-frontend')
+        });
+    },
+    app: function () {
+        this.template('_package.json', 'package.json',{'projectName':this.projectName});
+    }
+});
+
+module.exports = Generator;
